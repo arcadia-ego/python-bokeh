7769196fcabf2eda2ad742e6c20d8661217b0111
arcadia-ego <electronics.crawford@gmail.com>
2018-06-20 10:48:32 -0700

    graph fun

--- src/draw.py
+++ src/draw.py
@@ -4,7 +4,11 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
+from graph import *
 
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
 N = 8
 node_indices = list(range(N))
 
@@ -15,14 +19,14 @@ graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
     start=[0]*N,
     end=node_indices)
 
 ### start of layout code
-circ = [i*2*math.pi/8 for i in node_indices]
+circ = [i*2*math.pi/N for i in node_indices]
 x = [math.cos(i) for i in circ]
 y = [math.sin(i) for i in circ]
 
